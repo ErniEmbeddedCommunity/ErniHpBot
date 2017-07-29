f03a9ef7a762ec08a2b845400f075730700b38a8
Victor Perez <metalblueberry@gmail.com>
2017-07-29 17:50:27 +0200

    implemented TUser, there is still a few things to be done

--- ChatHandler.py
+++ ChatHandler.py
@@ -5,6 +5,7 @@ import time
 import telepot
 import ledController as led
 import tempfile
+import TelegramUser
 
 from telepot.loop import MessageLoop
 from telepot.delegate import pave_event_space, per_chat_id, create_open
@@ -19,6 +20,7 @@ class MessageCounter(telepot.helper.ChatHandler):
     Sample class to show how to use ChatHandler.
     Is no being used.
     """
+
     def __init__(self, *args, **kwargs):
         super(MessageCounter, self).__init__(*args, **kwargs)
         self._count = 0
@@ -28,7 +30,7 @@ class MessageCounter(telepot.helper.ChatHandler):
         self.sender.sendMessage("Welcome")
 
     def on_chat_message(self, msg):
-        """handle new msg.""" 
+        """handle new msg."""
         self._count += 1
         self.sender.sendMessage(self._count)
         self.sender.sendMessage(msg)
@@ -39,7 +41,7 @@ class MessageCounter(telepot.helper.ChatHandler):
     #     self.sender.sendMessage("closed")
     #     telepot.helper.ChatHandler.on_close(self,event)
 
-    def on__idle(self,event):
+    def on__idle(self, event):
         """Do something on idle"""
         self.sender.sendMessage("dispose")
         super().on__idle(event)
@@ -47,44 +49,50 @@ class MessageCounter(telepot.helper.ChatHandler):
 
 class PrivateUserChat(telepot.helper.ChatHandler):
     """Handles the private msgs"""
+
     def __init__(self, *args, **kwargs):
         super(PrivateUserChat, self).__init__(*args, **kwargs)
 
     def open(self, initial_msg, seed):
         """Do something when the first msg arrives."""
-        self._user = User.create_user_by_Id(initial_msg["from"])
+        # self._user = User.create_user_by_Id(initial_msg["from"])
 #        self.sender.sendMessage("Welcome to this private chat")
 
     def on_chat_message(self, msg):
-        """handle new msg.""" 
-        self._user.update_telegram_user(msg["from"])
+        """handle new msg."""
+        # self._user.update_telegram_user(msg["from"])
+        self.user = TelegramUser.TUser(msg["from"])
+        privilegesset = self.user.privileges
+        if privilegesset == None:
+            self.user.privileges = set()
         try:
-            if "voice" in msg:  
+            if "voice" in msg:
                 if not os.path.exists("voice"):
                     os.makedirs("voice")
                 voiceFile = self.bot.getFile(msg["voice"]["file_id"])
-                self.sender.sendMessage("Playing your audio over radio at " + str(FmPlayer.freq) + " Hz")
-                self.bot.download_file(voiceFile["file_id"],"voice/" + str(voiceFile["file_id"]) + ".ogg")
-                FmPlayer.play_file("voice/" + str(voiceFile["file_id"]) + ".ogg")
+                self.sender.sendMessage(
+                    "Playing your audio over radio at " + str(FmPlayer.freq) + " Hz")
+                self.bot.download_file(
+                    voiceFile["file_id"], "voice/" + str(voiceFile["file_id"]) + ".ogg")
+                FmPlayer.play_file(
+                    "voice/" + str(voiceFile["file_id"]) + ".ogg")
             if "text" in msg:
                 if msg["text"] == "Star":
-                    self.sender.sendMessage("May the force be with you at " + str(FmPlayer.freq) + " Hz")
-                    FmPlayer.play_file("fm_transmmiter/star_wars.wav", delete_after_convert=False, delete_after_play=False )
+                    self.sender.sendMessage(
+                        "May the force be with you at " + str(FmPlayer.freq) + " Hz")
+                    FmPlayer.play_file(
+                        "fm_transmmiter/star_wars.wav", delete_after_convert=False, delete_after_play=False)
                 if msg["text"] == "/User":
-                    self._user.privileges.add("User")
+                    self.user.privileges.add("User")
                     self.sender.sendMessage("You are user now")
                 if msg["text"].startswith("/Admin"):
-                    if msg["text"].endswith("erni"):
-                        self._user.privileges.add("Admin")
-                        self.sender.sendMessage("You are admin now")
-                    else:
-                        self.sender.sendMessage("Wrong password")
-
+                    self.user.privileges.add("Admin")
+                    self.sender.sendMessage("You are admin now")
                 if msg["text"] == "/CanI":
-                    self.sender.sendMessage(",".join(self._user.privileges))
-                if msg["text"] == "/toGroup":
-                    for groupId in self._user.groups:
-                        self.bot.sendMessage(groupId,str(self._user._telegram_user["username"]))
+                    self.sender.sendMessage(",".join(self.user.privileges))
+                if msg["text"] == "/info":
+                    for key, value in iter(self.user):
+                        self.sender.sendMessage(key + ": " + str(value))
                 if msg["text"] == "/led":
                     self.sender.sendMessage(
                         'Here are the led controllers',
@@ -100,26 +108,30 @@ class PrivateUserChat(telepot.helper.ChatHandler):
                     led.on()
                     self.sender.sendMessage("Led ON")
                 if msg["text"] == "/off" or msg["text"] == "LED OFF":
-                    led.off()    
+                    led.off()
                     self.sender.sendMessage("Led OFF")
                 # self.sender.sendMessage(self._user.get_last_msg() +".")
-                self._user.set_last_msg(msg["text"])
-        
+                self.user.last_msg = msg["text"]
+                history = self.user.history
+                if history == None:
+                    self.user.history = list()
+                self.user.history.append(msg["text"])
+                self.sender.sendMessage(self.user.history[-1])
+
         except telepot.exception.TelegramError as err:
             print(err)
 
-    def on__idle(self,event):
+    def on__idle(self, event):
         """Do something on idle"""
         # self.sender.sendMessage("closed")
-        User.dispose_user_by_id(self.chat_id)
+        # User.dispose_user_by_id(self.chat_id)
         super().on__idle(event)
 
 
-
-
 class GroupChat(telepot.helper.ChatHandler):
-    
+
     """Handles the group msgs"""
+
     def __init__(self, *args, **kwargs):
         super(GroupChat, self).__init__(*args, **kwargs)
         self._users = list()
@@ -130,17 +142,20 @@ class GroupChat(telepot.helper.ChatHandler):
         self._chat = Chat.create_chat_by_id(initial_msg["chat"])
 
     def on_chat_message(self, msg):
-        """handle new msg.""" 
-        #for user in _users:
+        """handle new msg."""
+        # for user in _users:
         self._chat.update_chat(msg["chat"])
         self._chat.add_user(msg["from"])
         current_user = self.add_user_to_group_list(msg["from"])
         if current_user.checkForPrivileges("User"):
-            if "voice" in msg: 
+            if "voice" in msg:
                 voiceFile = self.bot.getFile(msg["voice"]["file_id"])
-                self.sender.sendMessage("Playing your audio over radio at " + str(FmPlayer.freq) + " Hz")
-                self.bot.download_file(voiceFile["file_id"],"voice/" + str(voiceFile["file_id"]) + ".ogg")
-                FmPlayer.play_file("voice/" + str(voiceFile["file_id"]) + ".ogg")
+                self.sender.sendMessage(
+                    "Playing your audio over radio at " + str(FmPlayer.freq) + " Hz")
+                self.bot.download_file(
+                    voiceFile["file_id"], "voice/" + str(voiceFile["file_id"]) + ".ogg")
+                FmPlayer.play_file(
+                    "voice/" + str(voiceFile["file_id"]) + ".ogg")
             if "text" in msg:
                 if msg["text"] == "/led":
                     self.sender.sendMessage(
@@ -157,10 +172,9 @@ class GroupChat(telepot.helper.ChatHandler):
                     led.on()
                     self.sender.sendMessage("Led ON")
                 if msg["text"] == "/off" or msg["text"] == "LED OFF":
-                    led.off()    
+                    led.off()
                     self.sender.sendMessage("Led OFF")
-           
-        
+
     def add_user_to_group_list(self, user_msg):
         for user in self._users:
             if user.id() == user_msg["id"]:
@@ -177,7 +191,7 @@ class GroupChat(telepot.helper.ChatHandler):
     #     self.sender.sendMessage("closed")
     #     telepot.helper.ChatHandler.on_close(self,event)
 
-    def on__idle(self,event):
+    def on__idle(self, event):
         """Do something on idle"""
 #        self.sender.sendMessage("dispose")
         Chat.dispose_chat_by_id(self.chat_id)
@@ -192,7 +206,7 @@ bot = telepot.DelegatorBot(TOKEN, [
     pave_event_space()(
         per_chat_id(types='private'), create_open, PrivateUserChat, timeout=TIMEOUT),
     pave_event_space()(
-        per_chat_id(types=['supergroup','group']), create_open, GroupChat, timeout=TIMEOUT),
+        per_chat_id(types=['supergroup', 'group']), create_open, GroupChat, timeout=TIMEOUT),
 ])
 MessageLoop(bot).run_as_thread()
 
--- TelegramUser.py
+++ TelegramUser.py
@@ -1,11 +1,222 @@
 """
-This file contais the definition of TUser class
+This file contais the definition of TUser class and helpers to
+abstract the communication with the database
 """
-from telepot.loop import MessageLoop
+import redis as db
+import pickle
 
-class TUser(MessageLoop):
+CONN = db.StrictRedis()
+
+
+def redis_decode(inbytes):
+    """Helper function to decode bytes from database."""
+    return inbytes.decode("utf-8")
+
+
+class TUser():
     """handles the data for each user"""
-    def __init__(self):
-        self.Name = ""
-        self.TelegramID = 0
-        self.UserName = ""
+
+    def __init__(self, telegram_user):
+        self.id = telegram_user["id"]
+        for key, value in telegram_user.items():
+            self[key] = value
+
+    def __setattr__(self, name, value):
+        if name == "id":
+            self.__dict__["id"] = value
+            CONN.set("telegram_users_id", value)
+            # self.pickle_set("telegram_users_id", value)
+        else:
+            if isinstance(value, set):
+                RedisSet(self.redis_prefix(name), value)
+            if isinstance(value, list):
+                RedisList(self.redis_prefix(name), value)
+            elif isinstance(value, str):
+                CONN.set(self.redis_prefix(name), value)
+            else:
+                raise NotImplementedError(
+                    "Type of: " + type(value).__name__ + " is not implemented yet.")
+
+    def __getattr__(self, name):
+        # byteValue = self.pickle_get(self.redis_prefix(name))
+        # return byteValue
+        keytype = CONN.type(self.redis_prefix(name)).decode("utf-8")
+        if keytype == "none":
+            return None
+        elif keytype == "string":
+            byte_value = CONN.get(self.redis_prefix(name))
+            try:
+                float_value = float(byte_value)
+                return float_value
+            except ValueError:
+                pass
+            try:
+                int_value = int(byte_value)
+                return int_value
+            except ValueError:
+                pass
+            str_value = str(byte_value.decode("utf-8"))
+            return str_value
+        elif keytype == "set":
+            return RedisSet(self.redis_prefix(name))
+        elif keytype == "list":
+            return RedisList(self.redis_prefix(name))
+        else:
+            raise NotImplementedError(
+                "Key type " + str(keytype) + " is not supported.")
+
+    def __dir__(self):
+        attr = iter(self)
+        return attr
+
+    def __iter__(self):
+        long_keys = map(redis_decode, CONN.keys(self.redis_prefix("*")))
+        for long_key in long_keys:
+            key = long_key[long_key.find('_') + 1:]
+            try:
+                yield(key, self[key])
+            except UnicodeDecodeError as error:
+                pass
+                # print("invalid key in database: " + key + " " + str(error))
+
+    def __getitem__(self, name):
+        return self.__getattr__(name)
+
+    def __setitem__(self, name, value):
+        self.__setattr__(name, value)
+
+    def redis_prefix(self, attr):
+        """Generates the prefix for a desired attribute."""
+        return str(self.id) + "_" + attr
+    # pickle was an idea to store the data in the database
+    # independently of the data type
+    # Now I think it's preferable to use only raw strings
+    # to allow external programms to read the database
+
+    @staticmethod
+    def pickle_sadd(key, value):
+        """NOT IN USE: stores the value as a pickle serialized data."""
+        CONN.sadd(key, pickle.dumps(value))
+
+    @staticmethod
+    def pickle_set(key, value):
+        """NOT IN USE: stores the value as a pickle serialized data."""
+        CONN.set(key, pickle.dumps(value))
+
+    @staticmethod
+    def pickle_get(key):
+        """NOT IN USE: recover the value as a pickle serialized data."""
+        value = CONN.get(key)
+        if value != None:
+            # return value
+            return pickle.loads(value)
+        else:
+            return None
+
+
+class RedisSet:
+    """
+    allows the user to interact with redis set as if it were
+    a python set.
+    """
+
+    def __init__(self, key, setvalues=set()):
+        self.key = key
+        self._identification_key = "__redis_set__"
+        CONN.sadd(key, self._identification_key)
+        for value in setvalues:
+            self.add(value)
+
+    def add(self, value):
+        if value == self._identification_key:
+            Exception("this is an internal value.")
+        CONN.sadd(self.key, value)
+
+    def remove(self, value):
+        if value == self._identification_key:
+            Exception("this is an internal value.")
+        CONN.srem(self.key, value)
+
+    def clear(self):
+        CONN.delete(self.key)
+        CONN.sadd(self.key, self._identification_key)
+
+    def __iter__(self):
+        members = set(map(redis_decode, CONN.smembers(self.key)))
+        members.remove(self._identification_key)
+        for member in members:
+            yield member
+
+    def __repr__(self):
+        return super().__repr__() + " " + self.__str__()
+
+    def __str__(self):
+        return "[" + ",".join(map(str, iter(self))) + "]"
+
+
+class RedisList:
+    """
+    allows the user to interact with redis list as if it were
+    a python list.
+    """
+
+    def __init__(self, key, setvalues=list()):
+        self.key = key
+        self._identification_key = "__redis_list__"
+        CONN.lrem(key, 1, self._identification_key)
+        CONN.lpush(key, self._identification_key)
+        for value in setvalues:
+            self.append(value)
+
+    def append(self, value):
+        if value == self._identification_key:
+            Exception("this is an internal value.")
+        CONN.rpush(self.key, value)
+
+    def remove(self, value, count=1):
+        if value == self._identification_key:
+            Exception("this is an internal value.")
+        CONN.lrem(self.key, count, value)
+
+    def rpop(self):
+        return redis_decode(CONN.rpop(self.key))
+
+    def lpop(self):
+        return redis_decode(CONN.lpop(self.key))
+
+    def rpush(self, value):
+        CONN.rpush(self.key, value)
+
+    def lpush(self, value):
+        CONN.lpush(self.key, value)
+
+    def clear(self):
+        CONN.delete(self.key)
+        CONN.lpush(self.key, self._identification_key)
+
+    def __iter__(self):
+        # members = list(map(redis_decode, CONN.lrange(self.key, 0, -1)))
+        members = self[0:-1]
+        members.remove(self._identification_key)
+        for member in members:
+            yield member
+
+    def __setitem__(self, item, value):
+        CONN.lset(self.key, item, value)
+
+    def __delitem__(self, item):
+        CONN.lset(self.key, item, self._identification_key)
+        CONN.lrem(self.key, -1, self._identification_key)
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            return list(map(redis_decode, CONN.lrange(self.key, index.start, index.stop)))
+        else:
+            lrange = CONN.lrange(self.key, index, index)
+            return redis_decode(lrange[0])
+
+    def __repr__(self):
+        return super().__repr__() + " " + self.__str__()
+
+    def __str__(self):
+        return "[" + ",".join(map(str, iter(self))) + "]"
